e56e10757acfc373934300a06b1bbe965af49ad6
alparslan <alparslanorgnl@gmail.com>
2024-11-23 16:14:59 +0300

    caps edited

--- app/caps/page.js
+++ app/caps/page.js
@@ -6,9 +6,11 @@ import Link from "next/link";
 
 export default async function Page() {
 
+  const linkClasses = "text-[#FFC96B] font-bold hover:text-gray-500";
+
     const jsxElements = [
         <nav className="hidden md:flex space-x-4">
-          <Link key="home" href="#" className="hover:text-gray-500">CAPS</Link>
+          <Link key="home" href="#" className={linkClasses}>CAPS</Link>
         </nav>
       ];
 
